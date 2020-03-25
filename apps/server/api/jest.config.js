b6e46845b0f7fc8f8f02c6080c994d58c15a548b
Andreas Oxinos <oxinosa@gmail.com>
2020-03-25 15:08:17 +0100

    Initial Setup

---
+++ apps/server/api/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'server-api',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/server/api'
+};
---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Social</h1>
+  </div>
+);
